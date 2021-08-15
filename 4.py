c2c11165faefa4177fb3d4935cf521d71cb5ef9d
Kotelnikova Elena <ekotelnicova@gmail.com>
2021-08-15 22:39:58 +0300

    Выполнила дз к уроку № 3

---
+++ 4.py
@@ -0,0 +1,5 @@
+def my_func(x, y):
+    return x ** y
+
+
+print(my_func(float(input('Введите вещественное число: ')), int(input('Введите отрицательное целое число: '))))
---
+++ 6.py
@@ -0,0 +1,9 @@
+small_letter = (str.lower(input('Введите слово маленькими латинскими буквами: ')))
+
+
+def int_func():
+    return small_letter
+
+
+res = int_func()
+print(str.capitalize(res))
